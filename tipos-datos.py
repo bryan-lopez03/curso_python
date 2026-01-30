9691598aa92eeb7fdde51112e9e9cea09e739ab8
Bryan Lopez <nexara.lights@gmail.com>
2026-01-30 11:34:33 -0500

    Tipos de datos.py

---
+++ tipos-datos.py
@@ -0,0 +1,10 @@
+cadena="bryan lopez"
+caracter='L'
+edad= 26
+precio= 17.50
+soy_la_bestia= False
+print(type(cadena)) 
+print(type(caracter)) 
+print(type(edad))
+print(type(precio))
+print(type(soy_la_bestia))
